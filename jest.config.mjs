d913a3da03e0aab8720ac60bd5f0eab6bed104b1
dhiraja harwani <dhiraj.harwani@tatvasoft.com>
2025-05-07 11:55:19 +0530

    updated

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,25 +1,26 @@
 export default {
-    preset: 'ts-jest/presets/default-esm',
-    testEnvironment: 'node',
-    extensionsToTreatAsEsm: ['.ts'],
-    transform: {
-      '^.+\\.ts$': ['ts-jest', { useESM: true }],
+  preset: "ts-jest/presets/default-esm",
+  testEnvironment: "node",
+  extensionsToTreatAsEsm: [".ts"],
+  testMatch: ["**/__test__/**/*.ts"],
+  transform: {
+    "^.+\\.ts$": ["ts-jest", { useESM: true }],
+  },
+  globals: {
+    "ts-jest": {
+      useESM: true,
     },
-    globals: {
-      'ts-jest': {
-        useESM: true,
-      },
+  },
+  moduleNameMapper: {
+    "^(\\.{1,2}/.*)\\.js$": "$1",
+  },
+  collectCoverage: true,
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
     },
-    moduleNameMapper: {
-      '^(\\.{1,2}/.*)\\.js$': '$1',
-    },
-    collectCoverage: true,
-    coverageThreshold: {
-      global: {
-        branches: 80,
-        functions: 80,
-        lines: 80,
-        statements: 80,
-      },
-    }
-  }
+  },
+};
--- src/scripts/runSeeders.ts
+++ src/scripts/runSeeders.ts
@@ -1,13 +1,13 @@
-import { seed as seedUsers } from '../seeders/seed_users';
-import { dbClient } from '../config';
+// import { seed as seedUsers } from '../seeders/seed_users';
+// import { dbClient } from '../config';
 
-(async () => {
-    try {
-        await seedUsers();
-        console.log('✅ Seeding completed');
-    } catch (err) {
-        console.error('❌ Seeding failed', err);
-    } finally {
-        await dbClient.end();
-    }
-})();
+// (async () => {
+//     try {
+//         await seedUsers();
+//         console.log('✅ Seeding completed');
+//     } catch (err) {
+//         console.error('❌ Seeding failed', err);
+//     } finally {
+//         await dbClient.end();
+//     }
+// })();
